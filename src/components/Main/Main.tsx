6d55ded7a601772c2fa076fa007813d37b2bbecd
breznovic <slymarkel@gmail.com>
2023-01-24 15:49:25 +0300

    Main

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,10 +1,35 @@
+import { useRef } from 'react'
+import { AiOutlineArrowDown } from 'react-icons/ai'
+import senor from '../../assets/senor.webp'
 import classes from './main.module.css'
 
-function Main() {
-    return (
-        <div>
-    
-        </div>)
+
+const Main = () => {
+  const titleRef = useRef()
+
+  return (
+    <section id="about" className={classes.container}>
+      <div className={classes.wrapper}>
+        <div className={classes.left}>
+          <h2 className={classes.title}>Hi, I am Front-end Developer</h2> 
+          <h4 className={classes.subtitle}>Front-end Developer</h4>
+          <p className={classes.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Nemo, asperiores et? Neque iusto iure, perspiciatis excepturi similique
+            assumenda modi laboriosam reiciendis deserunt cupiditate
+            facilis inventore minima tempora quidem ullam laborum!
+          </p>
+          <div className={classes.buttons}>
+            <button>Contact me</button>
+            <button className={classes.seeMoreBtn}>See more <AiOutlineArrowDown /></button>
+          </div>
+        </div>
+        <div className={classes.right}>
+          <img src={senor} />
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Main
