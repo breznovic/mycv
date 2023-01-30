adfeedfb5a4c73d8ee75c8da19761465434f5cfc
breznovic <slymarkel@gmail.com>
2023-01-30 13:24:38 +0300

    Contacts layout

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -7,12 +7,12 @@ const Skills = () => {
             <div className={classes.wrapper}>
                 <div className={classes.titles}>
                     <h5 className={classes.subtitle}>What I offer</h5>
-                    <h2 className={classes.title}>Services</h2>
+                    <h2 className={classes.title}>My skills</h2>
                 </div>
                 <div className={classes.cards}>
                     <div className={classes.card}>
                         <div className={classes.top}>
-                            <span>Web Development <div className={classes.circles}><div className={classes.circle} /> <div className={classes.circle} /> <div className={classes.circle} /></div></span>
+                            <span>HTML/CSS <div className={classes.circles}><div className={classes.circle} /> <div className={classes.circle} /> <div className={classes.circle} /></div></span>
                             <i className="ri-code-s-slash-fill"></i>
                         </div>
                         <div className={classes.bottom}>
@@ -21,7 +21,7 @@ const Skills = () => {
                     </div>
                     <div className={classes.card}>
                         <div className={classes.top}>
-                            <span>Web Design <div className={classes.circles}><div className={classes.circle} /> <div className={classes.circle} /> <div className={classes.circle} /></div></span>
+                            <span>React <div className={classes.circles}><div className={classes.circle} /> <div className={classes.circle} /> <div className={classes.circle} /></div></span>
                             <i className="ri-pencil-line"></i>
                         </div>
                         <div className={classes.bottom}>
@@ -29,7 +29,7 @@ const Skills = () => {
                     </div>
                     <div className={classes.card}>
                         <div className={classes.top}>
-                            <span>Backend <div className={classes.circles}><div className={classes.circle} /> <div className={classes.circle} /> <div className={classes.circle} /></div></span>
+                            <span>TypeScript <div className={classes.circles}><div className={classes.circle} /> <div className={classes.circle} /> <div className={classes.circle} /></div></span>
                             <i className="ri-braces-line"></i>
                         </div>
                         <div className={classes.bottom}>
@@ -38,7 +38,7 @@ const Skills = () => {
                     </div>
                     <div className={classes.card}>
                         <div className={classes.top}>
-                            <span>Graphics Design <div className={classes.circles}><div className={classes.circle} /> <div className={classes.circle} /> <div className={classes.circle} /></div></span>
+                            <span>Redux Toolkit <div className={classes.circles}><div className={classes.circle} /> <div className={classes.circle} /> <div className={classes.circle} /></div></span>
                             <i className="ri-image-line"></i>
                         </div>
                         <div className={classes.bottom}>
@@ -47,7 +47,7 @@ const Skills = () => {
                     </div>
                     <div className={classes.card}>
                         <div className={classes.top}>
-                            <span>SEO <div className={classes.circles}><div className={classes.circle} /> <div className={classes.circle} /> <div className={classes.circle} /></div></span>
+                            <span>Unit-testing <div className={classes.circles}><div className={classes.circle} /> <div className={classes.circle} /> <div className={classes.circle} /></div></span>
                             <i className="ri-search-eye-line"></i>
                         </div>
                         <div className={classes.bottom}>
@@ -56,7 +56,7 @@ const Skills = () => {
                     </div>
                     <div className={classes.card}>
                         <div className={classes.top}>
-                            <span>Desktop Development <div className={classes.circles}><div className={classes.circle} /> <div className={classes.circle} /> <div className={classes.circle} /></div></span>
+                            <span>Node.js <div className={classes.circles}><div className={classes.circle} /> <div className={classes.circle} /> <div className={classes.circle} /></div></span>
                             <i className="ri-computer-line"></i>
                         </div>
                         <div className={classes.bottom}>
