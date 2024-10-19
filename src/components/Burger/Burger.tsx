a94c2b9471e5aa2481740f1678ae418295389d82
breznovic <slymarkel@gmail.com>
2024-10-19 18:59:58 +0300

    feat: add Navbar

---
+++ src/components/Burger/Burger.tsx
@@ -0,0 +1,30 @@
+import s from "./Burger.module.css";
+
+const Burger = () => {
+  return (
+    <>
+      <label>
+        <input type="checkbox" />
+        <span className={s.menu}>
+          <span className={s.burger}></span>
+        </span>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#skills">My skills</a>
+          </li>
+          <li>
+            <a href="#works">My Apps</a>
+          </li>
+          <li>
+            <a href="#contacts">Contacts</a>
+          </li>
+        </ul>
+      </label>
+    </>
+  );
+};
+
+export default Burger;
---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,42 @@
+
+import Burger from "../Burger/Burger";
+import s from "./Nav.module.css";
+
+const Navbar = () => {
+  const scrollToTop = () => {
+    window.scrollTo(0, 0);
+  };
+
+  return (
+    <div className={s.container}>
+      <div className={s.wrapper}>
+        <div className={s.name}>
+          <h2 onClick={scrollToTop}>Evgeniy Berezkin</h2>
+        </div>
+        <div className={s.right}>
+          <ul className={s.list}>
+            <li className={s.listItem}>
+              <a onClick={scrollToTop} href="#">
+                Home
+              </a>
+            </li>
+            <li className={s.listItem}>
+              <a href="#skills">My skills</a>
+            </li>
+            <li className={s.listItem}>
+              <a href="#works">My Apps</a>
+            </li>
+            <li className={s.listItem}>
+              <a href="#contacts">Contacts</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className={s.hide}>
+        <Burger />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
